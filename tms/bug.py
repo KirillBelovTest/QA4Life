4935216d91d87cc28be1674a880c58dc0cca7505
Kirill Belov <kirillbelovtest@gmail.com>
2026-02-22 23:26:05 +0400

    update

--- tms/bug.py
+++ tms/bug.py
@@ -1,3 +1,8 @@
+import tms.scenario as scn
+import tms.step as stp
+
 class Bug:
-    def __init__(self):
-        pass
+    def __init__(self, scenario: scn.Scenario, steps_to_reproduce: list[stp.Step]):
+        self.scenario = scenario
+        self.steps_to_reproduce = steps_to_reproduce
+        self.status = 'open'

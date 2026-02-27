714f9b436b828b898d7876b55421108342223e1b
Kirill Belov <kirillbelovtest@gmail.com>
2026-02-27 13:58:56 +0400

    Add bug management methods to Tester class

--- tms/tester.py
+++ tms/tester.py
@@ -3,6 +3,7 @@ import json
 import tms.tms as tms
 import tms.scenario as scn
 import tms.step as stp
+import tms.bug as bg
 
 class Tester:
     def __init__(self, tms: 'tms.TMS', name: str, level: int):
@@ -23,7 +24,8 @@ class Tester:
             'level': self.level,
             'current_scenario': self.current_scenario.name if isinstance(self.current_scenario, scn.Scenario) else '',
             'current_step': self.current_step.name if isinstance(self.current_step, stp.Step) else '',
-            'current_step_number': self.current_step_number
+            'current_step_number': self.current_step_number,
+            'current_step_bugs': [bug.to_dict() for bug in self.current_step.bugs] if self.current_step else []
         }
 
     def create_scenario(self, scenario_name: str):
@@ -63,3 +65,23 @@ class Tester:
             expected_result = self.current_scenario.steps[self.current_step_number].expected_result
             return expected_result == actual_result
         raise Exception(f'Step {self.current_step_number} out of range.')
+
+    def create_bug(self, steps_to_reproduce: list = None):
+        '''Тестировщик создаёт баг на текущем шаге.'''
+        if steps_to_reproduce is None:
+            steps_to_reproduce = [self.current_step]
+        
+        bug = bg.Bug(self.current_scenario, steps_to_reproduce)
+        self.current_step.bugs.append(bug)
+        return bug
+
+    def change_bug_status(self, bug_index: int, new_status: str):
+        '''Тестировщик меняет статус бага.'''
+        if bug_index < 0 or bug_index >= len(self.current_step.bugs):
+            raise Exception(f'Bug with index {bug_index} not found.')
+        
+        valid_statuses = ['open', 'closed', 'in_progress', 'wont_fix']
+        if new_status not in valid_statuses:
+            raise Exception(f'Invalid status. Must be one of: {valid_statuses}')
+        
+        self.current_step.bugs[bug_index].status = new_status
